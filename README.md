1454852eaaded25a04e398ab1289dcfeb8d48aa0
faruk ayaz <46282512+frkyz07@users.noreply.github.com>
2022-07-03 09:40:16 +0300

    - misunderstanding solved the real is homework finished

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # homework2
+## The real homework is in the SeacrhBoxTest.java the other classes and tested created because of the misunderstanding
 ## This is second week homework for the enuygun test automation bootcamp
-### You can find the explanation in Calculator.java and CalculatorTest.java file
--- enuygunweek2/src/test/java/org/example/SearchBoxTest.java
+++ enuygunweek2/src/test/java/org/example/SearchBoxTest.java
@@ -2,19 +2,35 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 class SearchBoxTest {
 
+    // now we are creating a method
     @Test
-    void searcboxpage() {
-        WebElement searchBoxPage = (WebElement) new SearchBox();
-        Select searchBox = (Select) searchBoxPage.findElement(By.id("searchBox"));
-        assertEquals(true, searchBox.isDisplayed());
+    public void SearchBoxTest() throws InterruptedException {
+
+        // we need to set our system properties for opening our browser
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "\\chromedriver.exe");
+        // we are creating a driver object to open our browser
+        ChromeDriver driver = new ChromeDriver();
+        // i want to see my chrome window in the maximize size
+        driver.manage().window().maximize();
+        // we are need to give the path that we want to go
+        driver.get("https://demoqa.com/elements");
+        // after i run the i always get the "click intercepted error"
+        // in order the fix the error i scroll down the page
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,250)");
+
+        // we give webtables section link to click
+        driver.findElement(By.xpath("//div//ul//li[@id='item-3'][1]")).click();
+        // in here we are giving the searchbox's xpath to the assertTrue assertion for comparing if it is displayed or not
+        assertTrue(driver.findElement(By.id("searchBox")).isDisplayed());
 
     }
 }
